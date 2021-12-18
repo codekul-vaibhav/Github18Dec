52f04ea74f63da84c76d6868b435e953ca5ccc15
codekul-vaibhav <codekul.vaibhav@gmail.com>
2021-12-18 11:45:49 +0530

    Added function

--- src/Sample.java
+++ src/Sample.java
@@ -1,7 +1,11 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hii");
-           System.out.println("Hii");
+        System.out.println("Hii");
+    }
+
+    public void add(){
+        System.out.println("IN add");
     }
 }
 
